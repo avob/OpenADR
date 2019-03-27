0ae5021819704ba20891aa781e4f1e4f0084721a
Bertrand Zanni <zanni.bertrand@gmail.com>
2019-03-27 19:01:08 +0100

    new custom certs

--- cert/gen.sh
+++ cert/gen.sh
@@ -2,14 +2,17 @@
 # This script setup a custom PKI architecture containing:
 # - custom certificate authority
 # - server vtn rsa/ecc
+# - client admin rsa
 # - client ven1 rsa
 # - client ven2 ecc
-# - client admin rsa
 # use openssl_client / curl to test ssl handshake between components:
 # openssl s_client -key ven1.oadr.com.key -cert ven1.oadr.com.crt -CAfile oadr.com.crt -connect vtn.oadr.com:8181
-# curl --key admin.oadr.com.key --cert admin.oadr.com.crt --cacert oadr.com.crt  \
-#	-d '{"username": "35:C2:79:32:5B:62:65:A7:AA:D3}' -H "Content-Type: application/json" -X POST https://localhost:8181/testvtn/Ven/ 
+# curl --key admin.oadr.com.key --cert admin.oadr.com.crt --cacert oadr.com.crt -H "Content-Type: application/json" -X GET https://localhost:8181/testvtn/Ven/ 
 ###################################################
+COUNTRY="FR"
+STATE="Paris"
+LOCALITY="Paris"
+ORGANIZATION="Avob"	
 
 CA_NAME="oadr.com"
 CLIENT_NAME="admin.oadr.com"
@@ -18,83 +21,112 @@ VEN1_NAME="ven1.oadr.com"
 VEN2_NAME="ven2.oadr.com"
 
 ###################################
-# CUSTOM CERTIFICATION AUTHORITY
+# HELPERS
 ###################################
-# Creating CA Certificate
-# create ca.key /ca.crt
-openssl req -nodes -new -x509  -keyout $CA_NAME.key -out $CA_NAME.crt -subj "/C=FR/ST=Paris/L=Paris/O=Avob/OU=Avob/CN=$CA_NAME"
+# write_openssl_config_file "out file name" "certificate CN" "certificate default_bits"
+write_openssl_config_file()
+{
+	FILENAME=$1
+	cat > $FILENAME <<- EOM
+	[ req ]
+	default_bits       = $3
+	distinguished_name = req_distinguished_name
+	req_extensions     = req_ext
+	[ req_distinguished_name ]
+	countryName                 = $COUNTRY
+	stateOrProvinceName         = $STATE
+	localityName               = $LOCALITY
+	organizationName           = $ORGANIZATION
+	commonName                 = $2
+	[ req_ext ]
+	subjectAltName =DNS:$2,DNS:localhost
+	EOM
+}
+# gen_rsa_certificate "key/csr/crt files name" "certificate CN" "in ca key/crt name" "serial" "days"
+gen_rsa_key_csr()
+{
+	OPENSSL_CONFIG_FILENAME="openssl.conf"
+	write_openssl_config_file $OPENSSL_CONFIG_FILENAME $2 2048
 
+	openssl req -out $1.csr -newkey rsa:2048 -nodes -keyout $1.key \
+		-subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/OU=$ORGANIZATION/CN=$2" -reqexts req_ext -config $OPENSSL_CONFIG_FILENAME
+
+	openssl x509 -req -days $5 -in $1.csr -CA $3.crt -CAkey $3.key -CAcreateserial -out $1.crt \
+		-extensions req_ext -extfile $OPENSSL_CONFIG_FILENAME
+
+	rm $OPENSSL_CONFIG_FILENAME
+}
+# gen_ecc_key_csr "out files name" "certificate CN"
+gen_ecc_key_csr()
+{
+	OPENSSL_CONFIG_FILENAME="openssl.conf"
+	write_openssl_config_file $OPENSSL_CONFIG_FILENAME $2 384
+
+	openssl req -out $1.csr -newkey ec:<(openssl ecparam -name secp384r1) -nodes -keyout $1.key \
+		-subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/OU=$ORGANIZATION/CN=$2" -reqexts req_ext -config $OPENSSL_CONFIG_FILENAME
+
+	openssl x509 -req -days $5 -in $1.csr -CA $3.crt -CAkey $3.key -CAcreateserial -out $1.crt \
+		-extensions req_ext -extfile $OPENSSL_CONFIG_FILENAME
+
+	rm $OPENSSL_CONFIG_FILENAME
+}
+# gen_pkcs12 "in/out key/crt/p12 name"
+gen_pkcs12()
+{
+	openssl pkcs12 -export  -inkey $1.key -in $1.crt -certfile $CA_NAME.crt -out $1.p12
+}
+# gen_oadr20b_fingerprint "in/out crt/fingerprint name"
+gen_oadr20b_fingerprint()
+{
+	openssl x509 -in $1.crt -fingerprint -sha256 -noout | cut -d'=' -f2 | cat | tail -c 30 > $1.fingerprint
+}
+# gen_oadr20a_fingerprint "in/out crt/fingerprint name"
+gen_oadr20a_fingerprint()
+{
+	openssl x509 -in $1.crt -fingerprint -sha1 -noout | cut -d'=' -f2 | cat | tail -c 30 > $1.fingerprint.oadr20a
+}
+# gen_selfsigned_key_crt "out key/crt name"
+gen_selfsigned_key_crt()
+{
+	openssl req -nodes -new -x509  -keyout $1.key -out $1.crt \
+		-subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/OU=$ORGANIZATION/CN=$1"
+}
 ###################################
-# CLIENT RSA
+# CA
 ###################################
-# Creating a Key and CSR for the Server
-# create $CLIENT_NAME.key / $CLIENT_NAME.csr
-openssl req -newkey rsa:2048 -nodes -keyout $CLIENT_NAME.key -out $CLIENT_NAME.csr -subj "/C=FR/ST=Paris/L=Paris/O=Avob/OU=Avob/CN=$CLIENT_NAME"
-
-# Signing Server Certificate with previously created CA.
-# $CLIENT_NAME.crt = $CLIENT_NAME.csr + $CA_NAME.crt + $CA_NAME.key
-openssl x509 -req -days 365 -in $CLIENT_NAME.csr -CA $CA_NAME.crt -CAkey $CA_NAME.key -set_serial 01 -out $CLIENT_NAME.crt
-
-# Compute client fingerprint
-openssl x509 -in $CLIENT_NAME.crt -fingerprint -sha256 -noout | cut -d'=' -f2 | cat | tail -c 30 > $CLIENT_NAME.fingerprint
-
-# Creating PKCS12 for the Client
-# $CLIENT_NAME.p12 = $CLIENT_NAME.key + $CLIENT_NAME.crt + $CA_NAME.crt
-openssl pkcs12 -export  -inkey $CLIENT_NAME.key -in $CLIENT_NAME.crt -certfile $CA_NAME.crt -out $CLIENT_NAME.p12
+gen_selfsigned_key_crt $CA_NAME
 
 ###################################
 # VTN RSA
 ###################################
-# Creating a Key and CSR for the Server
-# create $VTN_NAME-rsa..key / $VTN_NAME-rsa..csr
-openssl req -newkey rsa:2048 -nodes -keyout $VTN_NAME-rsa.key -out $VTN_NAME-rsa.csr -subj "/C=FR/ST=Paris/L=Paris/O=Avob/OU=Avob/CN=$VTN_NAME"
-
-# Signing Server Certificate with previously created CA.
-# $VTN_NAME-rsa. = $VTN_NAME-rsa..csr + $CA_NAME.crt + $CA_NAME.key
-openssl x509 -req -days 365 -in $VTN_NAME-rsa.csr -CA $CA_NAME.crt -CAkey $CA_NAME.key -set_serial 02 -out $VTN_NAME-rsa.crt
+gen_rsa_key_csr $VTN_NAME-rsa $VTN_NAME $CA_NAME 01 365
+gen_oadr20b_fingerprint $VTN_NAME-rsa
+gen_oadr20a_fingerprint $VTN_NAME-rsa
 
 ###################################
-# VTN ECC 256-bit
+# VTN ECC
 ###################################
-# Creating a Key and CSR for the Server
-# create $VTN_NAME.key / $VTN_NAME.csr
-openssl req -new -x509 -nodes -newkey ec:<(openssl ecparam -name secp384r1) -keyout $VTN_NAME.key -subj "/C=FR/ST=Paris/L=Paris/O=Avob/OU=Avob/CN=$VTN_NAME"
-openssl req -new -key $VTN_NAME.key -out $VTN_NAME.csr -sha256 -subj "/C=FR/ST=Paris/L=Paris/O=Avob/OU=Avob/CN=$VTN_NAME"
-
-# Signing Server Certificate with previously created CA.
-# $VTN_NAME.crt = $VTN_NAME.csr + $CA_NAME.crt + $CA_NAME.key
-openssl x509 -req -days 365 -in $VTN_NAME.csr -CA $CA_NAME.crt -CAkey $CA_NAME.key -set_serial 03 -out $VTN_NAME.crt
-
-# Compute VEN fingerprint
-openssl x509 -in $VTN_NAME.crt -fingerprint -sha256 -noout | cut -d'=' -f2 | cat | tail -c 30 > $VTN_NAME.fingerprint
+gen_ecc_key_csr $VTN_NAME-ecc $VTN_NAME $CA_NAME 02 365
+gen_oadr20b_fingerprint $VTN_NAME-ecc
+gen_oadr20a_fingerprint $VTN_NAME-ecc
 
 ###################################
-# VEN1 RSA
+# CLIENT
 ###################################
-# Creating a Key and CSR for the Server
-# create $VEN1_NAME.key / $VEN1_NAME.csr
-openssl req -newkey rsa:2048 -nodes -keyout $VEN1_NAME.key -out $VEN1_NAME.csr -subj "/C=FR/ST=Paris/L=Paris/O=Avob/OU=Avob/CN=$VEN1_NAME"
-
-# Signing Server Certificate with previously created CA.
-# $VEN1_NAME.crt = $VEN1_NAME.csr + $CA_NAME.crt + $CA_NAME.key
-openssl x509 -req -days 365 -in $VEN1_NAME.csr -CA $CA_NAME.crt -CAkey $CA_NAME.key -set_serial 04 -out $VEN1_NAME.crt
-
-# Compute VEN fingerprint
-openssl x509 -in $VEN1_NAME.crt -fingerprint -sha256 -noout | cut -d'=' -f2 | cat | tail -c 30 > $VEN1_NAME.fingerprint
+gen_rsa_key_csr $CLIENT_NAME $CLIENT_NAME $CA_NAME 03 365
+gen_pkcs12 $CLIENT_NAME
+gen_oadr20b_fingerprint $CLIENT_NAME
 
 ###################################
-# VEN2 ECC 256-bit
+# VEN1
 ###################################
-# Creating a Key and CSR for the Server
-# create $VEN2_NAME.key / $VEN2_NAME.csr
-openssl req -new -x509 -nodes -newkey ec:<(openssl ecparam -name secp384r1) -keyout $VEN2_NAME.key -subj "/C=FR/ST=Paris/L=Paris/O=Avob/OU=Avob/CN=$VEN2_NAME"
-openssl req -new -key $VEN2_NAME.key -out $VEN2_NAME.csr -sha256 -subj "/C=FR/ST=Paris/L=Paris/O=Avob/OU=Avob/CN=$VEN2_NAME"
+gen_rsa_key_csr $VEN1_NAME $VEN1_NAME $CA_NAME 04 365
+gen_oadr20b_fingerprint $VEN1_NAME
+gen_oadr20a_fingerprint $VEN1_NAME
 
-# Signing Server Certificate with previously created CA.
-# $VEN2_NAME.crt = $VEN2_NAME.csr + $CA_NAME.crt + $CA_NAME.key
-openssl x509 -req -days 365 -in $VEN2_NAME.csr -CA $CA_NAME.crt -CAkey $CA_NAME.key -set_serial 05 -out $VEN2_NAME.crt
-
-# Compute VEN fingerprint
-openssl x509 -in $VEN2_NAME.crt -fingerprint -sha256 -noout | cut -d'=' -f2 | cat | tail -c 30 > $VEN2_NAME.fingerprint
-
-
+###################################
+# VEN2
+###################################
+gen_ecc_key_csr $VEN2_NAME $VEN2_NAME $CA_NAME 05 365
+gen_oadr20b_fingerprint $VEN2_NAME
+gen_oadr20a_fingerprint $VEN2_NAME
